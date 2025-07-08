82e60a454ccb73802d4ea0d20337b10e0662020d
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-07-08 19:33:12 +0300

    current state of the robot arm.

---
+++ controllers/ure_can_grasper/build/release/ure_can_grasper.d
@@ -0,0 +1,7 @@
+build/release/ure_can_grasper.o: ure_can_grasper.c \
+  /Applications/Webots.app/Contents/include/controller/c/webots/distance_sensor.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/types.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/motor.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/position_sensor.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/robot.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/nodes.h
